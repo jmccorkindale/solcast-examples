907ada65a0d43bc3aba429a24a660a6f12a72413
Jared McCorkindale <jared.mccorkindale@anu.edu.au>
2018-04-03 13:27:21 +1000

    Add dotnet examples for create rooftop site and measurements

---
+++ dotnet/Solcast.Examples/CreateRooftopMeasurement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Solcast.Examples
+{
+    public class CreateRooftopMeasurement
+    {
+        public async Task Run()
+        {
+            var API_KEY = "[insert your api key here]";
+            var SOLCAST_API_URL = "https://api.solcast.com.au/rooftop_sites";
+            var RESOURCE_ID = "[insert the resource id of the rooftop site you are posting measurements for]";
+            
+            var measurement = new RooftopMeasurement(new DateTime(2018, 2, 2, 3, 30, 0), "PT5M", 1.2345);
+            var client = new CreateRooftopMeasurementClient(API_KEY, SOLCAST_API_URL);
+
+            await client.PostAsync(measurement, RESOURCE_ID);
+        }
+    }
+
+    public class RooftopMeasurement
+    {
+        public RooftopMeasurement(DateTime periodEnd, string period, double totalPower)
+        {
+            this.PeriodEnd = periodEnd;
+            this.Period = period;
+            this.TotalPower = totalPower;
+        }
+        
+        [JsonProperty(PropertyName = "period_end")]
+        public DateTime PeriodEnd { get; }
+        
+        [JsonProperty(PropertyName = "period")]
+        public string Period { get; }
+        
+        [JsonProperty(PropertyName = "total_power")]
+        public double TotalPower { get; }
+    }
+
+    public class CreateRooftopMeasurementClient
+    {
+        private readonly string ApiKey;
+        private readonly string Url;
+        
+        public CreateRooftopMeasurementClient(string apiKey, string url)
+        {
+            if (string.IsNullOrEmpty(apiKey)) throw new ArgumentNullException(nameof(apiKey));
+            if (string.IsNullOrEmpty(url)) throw new ArgumentNullException(nameof(url));
+
+            this.ApiKey = apiKey;
+            this.Url = url;
+        }
+
+        public async Task<HttpResponseMessage> PostAsync(RooftopMeasurement measurement, string resourceId)
+        {
+            if (measurement == null) throw new ArgumentNullException(nameof(measurement));
+            if (string.IsNullOrEmpty(resourceId)) throw new ArgumentNullException(nameof(resourceId));
+
+            using (var client = new HttpClient())
+            {
+                var json = JsonConvert.SerializeObject(measurement);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                return await client.PostAsync($"{Url}/{resourceId}/measurements?format=json&api_key={ApiKey}", content);
+            }
+        }
+    }
+}
---
+++ dotnet/Solcast.Examples/CreateRooftopSite.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Solcast.Examples
+{
+    public class CreateRooftopSite
+    {
+        public async Task<string> Run()
+        {
+            var API_KEY = "[insert your api key here]";
+            var SOLCAST_API_URL = "https://api.solcast.com.au/rooftop_sites";
+            
+            var site = new RooftopSite("My Site", -149.117, 35.2);
+            var client = new CreateRooftopSiteClient(API_KEY, SOLCAST_API_URL);
+
+            var response = await client.PostAsync(site);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
+    public class RooftopSite
+    {   
+        public RooftopSite(string name, double longitude, double latitude, double? azimuth = null, double? tilt = null)
+        {
+            Name = name;
+            Longitude = longitude;
+            Latitude = latitude;
+            Azimuth = azimuth;
+            Tilt = tilt;
+        }
+        
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; }
+        
+        [JsonProperty(PropertyName = "longitude")]
+        public double Longitude { get; }
+        
+        [JsonProperty(PropertyName = "latitude")]
+        public double Latitude { get; }
+        
+        [JsonProperty(PropertyName = "azimuth")]
+        public double? Azimuth { get; }
+        
+        [JsonProperty(PropertyName = "tilt")]
+        public double? Tilt { get; }
+    }
+
+    public class CreateRooftopSiteClient
+    {
+        private readonly string ApiKey;
+        private readonly string Url;
+        
+        public CreateRooftopSiteClient(string apiKey, string url)
+        {
+            if (string.IsNullOrEmpty(apiKey)) throw new ArgumentNullException(nameof(apiKey));
+            if (string.IsNullOrEmpty(url)) throw new ArgumentNullException(nameof(url));
+
+            this.ApiKey = apiKey;
+            this.Url = url;
+        }
+
+        public async Task<HttpResponseMessage> PostAsync(RooftopSite site)
+        {
+            if (site == null) throw new ArgumentNullException(nameof(site));
+
+            using (var client = new HttpClient())
+            {
+                var json = JsonConvert.SerializeObject(site);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                return await client.PostAsync($"{Url}?format=json&api_key={ApiKey}", content);
+            }
+        }
+    }
+}
